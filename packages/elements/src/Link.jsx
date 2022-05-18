5b8904b17ff2e41413f299f1419a42c05a975d27
Michael Chadwick <michael@interactstudio.co.uk>
2022-05-18 14:36:03 +0100

    feat: add as prop for links to allow button type

--- packages/elements/src/Link.jsx
+++ packages/elements/src/Link.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export function Link({ children, href }) {
-  return <a href={href}>{children}</a>;
+export function Link({ children, href, as }) {
+  return (
+    <a data-type={as} href={href}>
+      {children}
+    </a>
+  );
 }
 
 Link.propTypes = {
   children: PropTypes.node.isRequired,
   href: PropTypes.string.isRequired,
+  as: PropTypes.oneOf(["link", "button"]),
+};
+
+Link.defaultProps = {
+  as: "link",
 };
