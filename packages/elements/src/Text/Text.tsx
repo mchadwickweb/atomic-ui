47afb8bb6318d2a6f20baa9e9dfeb05c01f6ce68
Michael Chadwick <michael@interactstudio.co.uk>
2022-07-11 09:16:13 +0100

    convert Text to ts

---
+++ packages/elements/src/Text/Text.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Text.css";
+
+export interface TextProps {
+  children: React.ReactNode;
+  align: "left" | "center" | "right";
+}
+
+export function Text({ children, align = "left" }: TextProps) {
+  return <p data-text-align={align}>{children}</p>;
+}
