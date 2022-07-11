ccdb578e2a9c2dec1c6fa244dbba21c3bb4098e9
Michael Chadwick <michael@interactstudio.co.uk>
2022-07-11 13:05:56 +0100

    moved docs website to top level directory

---
+++ website/pages/components/text-field.tsx
@@ -0,0 +1,48 @@
+import { TextField } from "@atomic-ui/components";
+
+export default function TextFieldPage() {
+  return (
+    <>
+      <h1>Text Field</h1>
+      <h2>Examples</h2>
+      <p>
+        <strong>Simple text field with label</strong>
+      </p>
+      <TextField id="example" name="example" labelText="Example" type="text" />
+
+      <p>
+        <strong>Simple text field with hint</strong>
+      </p>
+      <TextField
+        id="example"
+        name="example"
+        labelText="Example"
+        type="text"
+        hint="This is a hint"
+      />
+
+      <p>
+        <strong>Simple text field with error</strong>
+      </p>
+      <TextField
+        id="example"
+        name="example"
+        labelText="Example"
+        type="text"
+        errorMessage="Uh oh!"
+      />
+
+      <p>
+        <strong>Simple text field with hint and error</strong>
+      </p>
+      <TextField
+        id="example"
+        name="example"
+        labelText="Example"
+        type="text"
+        hint="This is a hint"
+        errorMessage="Uh oh!"
+      />
+    </>
+  );
+}
